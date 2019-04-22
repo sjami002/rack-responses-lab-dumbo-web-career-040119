76d0be0b87869fe7c909ec971531c43d07d07b4c
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-22 19:56:18 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+ 
+  def call(env)
+    resp = Rack::Response.new
+    
+    curr_time = Time.now
+    if curr_time.hour >= 12
+      resp.write "Good Afternoon"
+    else
+      resp.write "Good Morning"
+    end
+    resp.finish
+  end
+ 
+end
